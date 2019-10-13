62f8e0a3f43ad0a2ec7d0238e190a7db73e73a37
root <duqrldudgns@gmail.com>
2019-10-13 23:56:59 +0900

    echo_client_server

---
+++ echo_client/main.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define BUFF_SIZE 1024
+
+void *send_fc(void *argumentPointer){
+    int client_socket = *reinterpret_cast<int*>(argumentPointer);
+    char message[BUFF_SIZE];
+    memset(message,0,sizeof(message));
+
+    while(true){
+
+        printf("Client : ");
+        fgets(message,BUFF_SIZE,stdin);
+        size_t echoStringLen = strlen(message);
+
+        ssize_t numBytes = send(client_socket,message, echoStringLen, 0);
+        if (numBytes == -1){
+            printf("send() error");
+            exit(1);
+        }
+        if(strcmp(message,"quit\n") == 0) exit(1);
+    }
+}
+
+void *recv_fc(void *argumentPointer){
+    int client_socket = *reinterpret_cast<int*>(argumentPointer);
+    char buffer[BUFF_SIZE];                            //make message recv buffer
+    memset(buffer,0,sizeof(buffer));
+
+    while(true){
+        ssize_t numBytes = recv(client_socket, buffer, BUFF_SIZE, 0);
+        if(numBytes == -1){
+            printf("recv fail \n");
+            exit(1);
+        }
+        printf("%d bytes read \n", int(numBytes));
+        printf("%s \n",buffer);
+        if(strcmp(buffer,"quit\n") == 0) exit(1);
+    }
+}
+
+void usage() {
+    printf("syntax: echo_client <server_ip> <port>\n");
+    printf("sample: echo_client 127.0.0.1 1996\n");
+}
+
+int main(int argc, char *argv[]){
+    if(argc != 3){
+        usage();
+        return -1;
+    }
+
+    int client_socket;
+    struct sockaddr_in server_addr;
+    pthread_t send_thread, recv_thread;
+
+    client_socket = socket(PF_INET,SOCK_STREAM,0);
+    if(client_socket == -1){
+        printf("socket make fail\n");
+        exit(1);
+    }
+
+    memset(&server_addr,0,sizeof(server_addr));
+
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    server_addr.sin_port        = htons(atoi(argv[2]));
+
+    if(connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+        printf("Can't connect\n");
+        close(client_socket);
+        exit(1);
+    }
+
+    pthread_create(&send_thread, nullptr, send_fc, static_cast<void*>(&client_socket));
+    pthread_create(&recv_thread, nullptr, recv_fc, static_cast<void*>(&client_socket));
+    pthread_join(send_thread, nullptr);
+    pthread_join(recv_thread, nullptr);
+
+    close(client_socket);
+    return 0;
+}
---
+++ echo_server/main.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <list>
+
+#define BUFF_SIZE 1024
+
+using namespace std;
+
+static std::list<int> lt;
+static std::list<int>::iterator iter;
+
+void *recv_fc(void *argumentPointer){
+    int client_socket = *reinterpret_cast<int*>(argumentPointer);
+    char buffer[BUFF_SIZE];
+    memset(buffer,0,sizeof(buffer));
+    size_t echoStringLen = strlen(buffer);
+
+    while(true){
+        ssize_t numBytes = recv(client_socket, buffer, BUFF_SIZE, 0);
+        if(numBytes == -1){
+            printf("recv fail \n");
+            exit(0);
+        }
+        printf("%s \n" ,buffer);
+
+        for(iter=lt.begin();iter !=lt.end();++iter)
+            send(client_socket,buffer, echoStringLen, 0);
+
+    }
+}
+
+void usage() {
+    printf("syntax: echo_server <port> (option)\n");
+    printf("sample: echo_server 1996 (-b)\n");
+}
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        usage();
+        return -1;
+    }
+
+    struct sockaddr_in server_addr, client_addr;
+    int server_socket, client_socket;                               //fd = socket
+    int len = sizeof(client_addr);
+    pthread_t recv_thread;
+    char temp[20];
+
+    server_socket = socket(AF_INET,SOCK_STREAM,0);
+    if(server_socket == -1){  //socket(IPv4, TCP, option 0)
+        printf("Server: can not Open Socket\n");
+        exit(0);
+    }
+
+    memset(&server_addr,0x00,sizeof(server_addr));          //server_Addr 을 NULL로 초기화
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(uint16_t(atoi(argv[1])));
+
+    if(bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr))< 0){
+        printf("Server: cat not bind local addrss\n");      //소켓을 할당 받으면 소켓에 주소, 프로토콜, 포트가 할당된다.
+        exit(0);                                        //bind(할당받을 socket, (sockaddr *)소켓의 주소정보, 구조체의 크기)
+    }
+
+    if(listen(server_socket,5) < 0){                            //listen(소켓, 접속가능한 클라이언트 수)
+        printf("Server: cat not listen connnect.\n");
+        exit(0);
+    }
+
+    printf("=====[PORT] : %d =====\n",atoi(argv[1]));
+    printf("Server : wating connection request.\n");
+
+
+    while(true){
+        lt.push_back(accept(server_socket, (struct sockaddr *)&client_addr, (socklen_t *)&len));
+        client_socket = lt.back();
+        printf("client_socket check : %d \n", client_socket );
+        if (lt.back() < 0){             // accept 기다리는 과정       accept(소켓, (sockaddr *)클라이언트의 주소를 저장할 구조체, 구조체의 크기)
+            printf("Server: accept failed\n");
+            exit(0);
+        }
+
+        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, temp, sizeof(temp));
+        printf("Server: %s client connect,\n", temp);
+
+        pthread_create(&recv_thread, nullptr, recv_fc, static_cast<void*>(&client_socket));
+
+        //pthread_join(recv_thread, nullptr);
+    }
+
+    return 0;
+}
